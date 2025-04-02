9a574edc6554efaf1fc2a7366cc34c28fe010869
johnyagithub <jonya1530@gmail.com>
2025-04-02 14:25:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="EN">
+ <head>
+		<title>dan-tech</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ </head>
+	<body>
+ 	<script type="text/javascript" src="https://www.dad.co.th//js/jquery-3.2.1.min.js"></script>
+		<script>
+			$(document).ready(function() {
+				parent.location.replace('views/home');
+			});
+		</script>
+ </body>
+</html>
